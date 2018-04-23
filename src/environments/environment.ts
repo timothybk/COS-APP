69f820c1cf64a61ac5506edd9f2fa4ae7c1965da
Tim Buckland-Kerr <timbk86@gmail.com>
2018-04-23 18:04:41 +1000

    add firestore and auth to modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAvUSIKSl75DMN-QwODMh6QH1eCj5a2gdk',
+    authDomain: 'cos-app-ef9c5.firebaseapp.com',
+    databaseURL: 'https://cos-app-ef9c5.firebaseio.com',
+    projectId: 'cos-app-ef9c5',
+    storageBucket: '',
+    messagingSenderId: '309610460066'
+  }
 };
